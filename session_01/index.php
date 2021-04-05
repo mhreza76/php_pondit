e7a16bb1d2aeab6f88e6fc5e776f5fef36d36207
Mahmudul Hasan Reza <reza@Mahmuduls-iMac.local>
2021-04-06 01:11:06 +0600

    first lecture

---
+++ session_01/index.php
@@ -0,0 +1,18 @@
+<?php
+define('MESSAGES', 'Getting started with PHP');
+echo MESSAGES;
+
+$str1 = "Using Double 'quotes' and single quotes";
+$str2 = 'Using Double "quotes" and single quotes';
+echo "<br />";
+echo $str1;
+echo "<br />";
+echo $str2;
+$num1 = 10;
+echo '<br>';
+echo 'use variable' . $num1;
+echo '<pre>';
+print_r("afdsfada
+asdfasfs
+            adfaf");
+echo '</pre>';
