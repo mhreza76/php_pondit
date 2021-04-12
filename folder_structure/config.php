4697f1b69f4279983bc5562b20670bfaf0ccad27
Mahmudul Hasan Reza <reza@Mahmuduls-iMac.local>
2021-04-12 06:41:54 +0600

    php folder structure for composer

---
+++ folder_structure/config.php
@@ -0,0 +1,4 @@
+<?php
+
+$app_root = $_SERVER['DOCUMENT_ROOT'] . '/php_pondit/folder_structure';
+include_once($app_root . '/vendor/autoload.php');
---
+++ folder_structure/public/index.php
@@ -0,0 +1,6 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . "/php_pondit/folder_structure/config.php");
+use App\Institute;
+
+
+?>
