3b904d19f2b01aebaa164cc6f2f5766dbab4679e
mhreza76 <rezabaiust@gmail.com>
2021-06-24 20:37:36 +0600

    Indexed Array

--- foreach/01_foreach.php
+++ foreach/01_foreach.php
@@ -3,11 +3,11 @@
 
 //Indexed Array
 $salaries = [
-  1000,
-  2000,
-  10000,
-  5500,
-  7000
+    1000,
+    2000,
+    10000,
+    5500,
+    7000
 ];
 
 foreach ($salaries as $salary){
@@ -20,4 +20,3 @@ foreach ($salaries as $index => $value){
     echo "$index : $value";
     echo '</br>';
 }
-
