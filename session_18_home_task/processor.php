a33e3aaea1172d87e848d07f28bb078b60fd938b
mhreza76 <rezabaiust@gmail.com>
2021-06-27 17:24:00 +0600

    full name validation

--- session_18_home_task/processor.php
+++ session_18_home_task/processor.php
@@ -23,16 +23,35 @@ if(strtoupper($_SERVER['REQUEST_METHOD']) == 'POST'){
         $full_name = $_POST['fullName'];
         $full_name_array = explode(' ',$full_name);
 
-        $count = 0;
-        foreach($full_name_array as $name){
-            if(has_alpha($name)){
-                $count++;
-                break;
+        $test = explode(',',$full_name);
+        echo "<pre>";
+            echo $test[0];
+        echo "</pre>";
+        if(!empty($strvalue)){
+            if(preg_match("/^([a-zA-Z. ]+)$/",$full_name)){
+                echo 'Valid name given.';
+            }else{
+                echo 'Invalid name given.';
             }
+
+
+//        $count = 0;
+//        foreach($full_name_array as $name){
+//            if(has_alpha($name)){
+//                $count++;
+//                break;
+//            }
+//        }
+//        if($count < 1){
+//            echo "Submitted Successfully";
+//        }
+
+
         }
-        if($count < 1){
-            echo "Submitted Successfully";
+        else{
+            echo "name can't be empty";
         }
+
     }
 }
 
